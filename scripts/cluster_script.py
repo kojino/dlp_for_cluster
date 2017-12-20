8190cecc4b6d578a93b24dcee4c2b0b62f0c4762
Kojin Oshiba <kojin.oshiba@gmail.com>
2017-12-20 14:07:37 -0500

    update cluster script

--- scripts/cluster_script.py
+++ scripts/cluster_script.py
@@ -11,67 +11,99 @@ from model import *
 def main(args):
 
     if args.data == "cora":
-        true_labels, features, graph = Data.Data.load_cora(rel_path='../')
+        true_labels, features, graph = DataT.DataT.load_cora(rel_path='../',multiclass=False)
         if not args.graph:
             graph = []
 
     np.random.seed(args.seed)
-    sigma = np.random.uniform(0.2,10)
-    theta = np.random.uniform(0.2,10,(1,features.shape[1]))
+    sigma = np.random.uniform(0.2,5)
+    theta = np.random.uniform(0.2,5,(1,features.shape[1]))
 
     labels, is_labeled, labeled_indices, unlabeled_indices \
     = utils.random_unlabel(true_labels,
                            unlabel_prob=1-float(args.labeled_percentage),
-                           hard=False,seed=args.seed)
+                           hard=False,multiclass=True,seed=args.seed)
     solution  = true_labels[unlabeled_indices]
 
     weights, graph = utils.rbf_kernel(features,
                                       s=sigma,
                                       G=graph,
                                       percentile=args.threshold_percentage)
+
     num_nodes = len(labels)
-    data, validation_data = Data.Data.prepare(labels,labeled_indices,true_labels)
+    num_classes = true_labels.shape[1]
 
-    if args.link_function == "rbf":
+    # crossval
+    np.random.seed(None)
+    num_labeled = len(labeled_indices)
+    num_unlabeled = len(unlabeled_indices)
+    p=float(args.crossval_p)
+    val_num_labeled = int(num_labeled*p)
+    val_num_unlabeled = int(num_unlabeled*p)
+    m=int(args.crossval_m)
+    for i in range(m):
+        val_labeled_indices_original = sorted(np.random.choice(labeled_indices, val_num_labeled))
+        val_unlabeled_indices_original = sorted(np.random.choice(unlabeled_indices, val_num_unlabeled))
+        val_indices_original = sorted(np.append(val_labeled_indices_original,val_unlabeled_indices_original))
+        val_labeled_indices = np.where(np.isin(val_indices_original,val_labeled_indices_original))[0]
+        val_unlabeled_indices = np.where(np.isin(val_indices_original,val_unlabeled_indices_original))[0]
+        val_labels = labels[val_indices_original]
+        val_is_labeled = is_labeled[val_indices_original]
+        val_solution = true_labels[val_unlabeled_indices_original]
+        val_features = features[val_indices_original]
+        val_weights = weights[np.ix_(val_indices_original,val_indices_original)]
+        val_graph = graph[np.ix_(val_indices_original,val_indices_original)]
+        val_true_labels = true_labels[val_indices_original]
+        val_num_nodes = len(val_indices_original)
 
-        dlp_rbf = DeepLP_RBF.DeepLP_RBF(num_nodes,
-                                        features,
-                                        graph,
-                                        sigma,
-                                        num_iter=args.num_iter,
-                                        lr=args.lr,
-                                        regularize=args.regularize,
-                                        loss_type=args.loss)
-        dlp_rbf.train(data,validation_data,args.num_epoch)
+        val_data, val_validation_data = DataT.DataT.prepare(val_labels,val_labeled_indices,val_true_labels,args.k,num_classes,args.num_samples,val_num_nodes)
 
-    if args.link_function == "wrbf":
+        if args.link_function == "rbf":
+            dlp_rbf = DeepLP_RBFT.DeepLP_RBFT(val_num_nodes,
+                                              num_classes,
+                                              val_features,
+                                              val_graph,
+                                              sigma,
+                                              num_iter=args.num_iter,
+                                              lr=args.lr,
+                                              regularize=args.regularize,
+                                              loss_type=args.loss,
+                                              graph_sparse=True)
+            dlp_rbf.train(val_data,val_validation_data,args.num_epoch)
 
-        dlp_wrbf = DeepLP_WeightedRBF.DeepLP_WeightedRBF(num_nodes,
-                                                         features,
-                                                         graph,
-                                                         sigma,
-                                                         theta,
-                                                         num_iter=args.num_iter,
-                                                         lr=args.lr,
-                                                         regularize=args.regularize,
-                                                         loss_type=args.loss)
-        dlp_wrbf_prediction,_ = dlp_wrbf.labelprop(validation_data)
+        if args.link_function == "wrbf":
+
+            dlp_wrbf = DeepLP_WeightedRBFT.DeepLP_WeightedRBFT(val_num_nodes,
+                                                             num_classes,
+                                                             val_features,
+                                                             val_graph,
+                                                             sigma,
+                                                             theta,
+                                                             num_iter=args.num_iter,
+                                                             lr=args.lr,
+                                                             regularize=args.regularize,
+                                                             loss_type=args.loss,
+                                                             graph_sparse=True)
+            dlp_wrbf.train(val_data,val_validation_data,args.num_epoch)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data",                 default="cora")
-    parser.add_argument("--graph",                default=0)
-    parser.add_argument("--k",                    default=1)
-    parser.add_argument("--labeled_percentage",   default=0.1)
-    parser.add_argument("--link_function",        default="rbf", help="rbf, wrbf")
-    parser.add_argument("--loss",                 default="mse", help="mse, log")
-    parser.add_argument("--lr",                   default=0.1)
-    parser.add_argument("--num_epoch",            default=1000)
-    parser.add_argument("--num_iter",             default=100)
-    parser.add_argument("--regularize",       default=0)
-    parser.add_argument("--seed",                 default=None)
-    parser.add_argument("--threshold_percentage", default=0)
+    parser.add_argument("--crossval_p",           default=1,      type=float, help="fraction to be sampled for cv, 1 means no split")
+    parser.add_argument("--crossval_m",           default=1,      type=int,   help="number of cv folds")
+    parser.add_argument("--data",                 default="cora",             help="dataset")
+    parser.add_argument("--graph",                default=0,      type=int,   help="use a given graph if 1, construct a graph if 0")
+    parser.add_argument("--k",                    default=1,      type=int,   help="leave-k-out loss")
+    parser.add_argument("--labeled_percentage",   default=0.1,    type=float, help="percentage of labeled nodes")
+    parser.add_argument("--link_function",        default="rbf",              help="link function g: rbf, wrbf")
+    parser.add_argument("--loss",                 default="mse",              help="type of loss function: mse, log")
+    parser.add_argument("--lr",                   default=0.1,    type=float, help="learning rate")
+    parser.add_argument("--num_epoch",            default=1000,   type=int,   help=" number of epochs to train the network, early stopping is always applied") #
+    parser.add_argument("--num_iter",             default=100,    type=int,   help="number of hidden layers, i.e. iterations of lp")
+    parser.add_argument("--num_samples",          default=10,     type=int,   help="number of data points for training")
+    parser.add_argument("--regularize",           default=0,      type=int,   help="0: no regularization")
+    parser.add_argument("--seed",                 default=None,   type=int,   help="seed for random parameter initialization")
+    parser.add_argument("--threshold_percentage", default=0,      type=float, help="if graph is constructed, dropping nodes under this percentile")
 
     args = parser.parse_args()
     main(args)
